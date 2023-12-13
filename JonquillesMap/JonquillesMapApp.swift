2dac487c84d83df43027c71c90bdd7359d8b3a07
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2023-12-13 16:48:47 +0200

    Add Xcode basic project

---
+++ JonquillesMap/JonquillesMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  JonquillesMapApp.swift
+//  JonquillesMap
+//
+//  Created by Florian DAVID on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct JonquillesMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
